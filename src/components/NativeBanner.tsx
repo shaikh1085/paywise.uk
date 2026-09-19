773c53056f41fc72c0c5f9a9c34303b6449d27e2
shaikh1085 <hr144329@gmail.com>
2026-09-20 03:39:56 +0500

    Create NativeBanner.tsx

---
+++ src/components/NativeBanner.tsx
@@ -0,0 +1,2 @@
+<script async="async" data-cfasync="false" src="https://pl31421676.profitableratecpmnetwork.com/156fd0702a5b1ca81ef6d1a6bd07d14f/invoke.js"></script>
+<div id="container-156fd0702a5b1ca81ef6d1a6bd07d14f"></div>
